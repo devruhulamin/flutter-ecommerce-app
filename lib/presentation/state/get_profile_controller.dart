bc47380b92e0e16c5e1133bed9d3820891238368
devruhulamin <pythonruhul@gmail.com>
2024-01-21 13:17:03 +0600

    get profile controller implement

--- lib/presentation/state/get_profile_controller.dart
+++ lib/presentation/state/get_profile_controller.dart
@@ -1,30 +1,28 @@
 import 'package:crafty_bay_ruhulaminjr/data/model/profile.dart';
 import 'package:crafty_bay_ruhulaminjr/data/service/newtwork_caller.dart';
 import 'package:crafty_bay_ruhulaminjr/data/utility/api_url.dart';
-import 'package:crafty_bay_ruhulaminjr/data/model/local_storage.dart';
 import 'package:get/get.dart';
 
-typedef IsProfileCompleted = bool;
-
-class GetProfileController extends GetxController {
+class ProfileDataController extends GetxController {
   bool _isloading = false;
+  Profile? _profile;
+  bool _isCompleted = false;
   bool get isLoading => _isloading;
-
-  Future<IsProfileCompleted> getProfileData() async {
+  Profile? get getProfileData => _profile;
+  bool get isProfileCompleted => _isCompleted;
+  Future<bool> readProfileData(String token) async {
     _isloading = true;
     update();
     try {
-      final localStorage = AuthLocalStorage();
-      final token = await localStorage.getToken();
       final response =
           await NetworkCaller().getRequiest(url: ApiUrl.baseUrl, token: token);
       if (response.isSuccess) {
         final profiledata = response.responseData['data'];
-        if (profiledata == null) {
-          return false;
+        if (profiledata.isEmpty) {
+          _isCompleted = false;
         } else {
-          final profile = Profile.fromJson(profiledata[0]);
-          await localStorage.saveProfileData(profile);
+          _profile = Profile.fromJson(profiledata);
+          _isCompleted = true;
           return true;
         }
       }
