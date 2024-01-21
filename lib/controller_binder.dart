048b87458898618e058551cb9095d4f68cb32867
devruhulamin <pythonruhul@gmail.com>
2024-01-21 13:23:06 +0600

    added splash screen route

--- lib/controller_binder.dart
+++ lib/controller_binder.dart
@@ -1,4 +1,6 @@
+import 'package:crafty_bay_ruhulaminjr/presentation/state/auth_controller.dart';
 import 'package:crafty_bay_ruhulaminjr/presentation/state/bottom_nav_bar_controller.dart';
+import 'package:crafty_bay_ruhulaminjr/presentation/state/profile_data_controller.dart';
 import 'package:crafty_bay_ruhulaminjr/presentation/state/send_email_otp_controller.dart';
 import 'package:crafty_bay_ruhulaminjr/presentation/state/verify_otp_controller.dart';
 import 'package:get/get.dart';
@@ -9,5 +11,7 @@ class ControllerBinder extends Bindings {
     Get.put(BottomNavBarController());
     Get.put(SendEmailOtpController());
     Get.put(VerifyOtpController());
+    Get.put(AuthController());
+    Get.put(ProfileDataController());
   }
 }
--- lib/presentation/ui/screens/auth/splash_screen.dart
+++ lib/presentation/ui/screens/auth/splash_screen.dart
@@ -1,5 +1,7 @@
+import 'package:crafty_bay_ruhulaminjr/presentation/state/auth_controller.dart';
 import 'package:crafty_bay_ruhulaminjr/presentation/ui/screens/auth/enter_email_scree.dart';
 import 'package:crafty_bay_ruhulaminjr/presentation/ui/utilities/app_logo.dart';
+import 'package:crafty_bay_ruhulaminjr/presentation/ui/widget/bottom_navigation_bar_home.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -13,9 +15,14 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   void go() {
     Future.delayed(
-      const Duration(seconds: 3),
-      () {
-        Get.offAll(() => EnterYourEmailScreen());
+      const Duration(seconds: 1),
+      () async {
+        final isLoggedIn = await Get.find<AuthController>().isLoggedIn();
+        if (isLoggedIn) {
+          Get.offAll(() => const BottomNavBarHome());
+        } else {
+          Get.offAll(() => EnterYourEmailScreen());
+        }
       },
     );
   }
